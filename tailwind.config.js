d2d7f9266f3a3ef8eb7464d1fc4c5a3877603c2a
Ahdremy <workwithadremy@gmail.com>
2023-01-31 16:18:51 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+
+    screens: {
+      xs: '480px',
+      sm: '600px',
+      md: '768px',
+      lg: '1020px',
+      xl: '1440px'
+    },
+
+    extend: {
+      fontFamily: {
+        sans:'Atkinson Hyperlegible, sans-serif'
+      },
+
+      colors: {
+        "Main": '#2d3748'
+      },
+
+    },
+  },
+  plugins: [],
+}
